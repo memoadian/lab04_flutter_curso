12d3b768236e1c115347616fa8e8fb46718ed332
Guillermo Canales <memoadian@gmail.com>
2019-11-19 22:11:21 -0600

    mandar data al servidor

--- lib/models_api/Pet.dart
+++ lib/models_api/Pet.dart
@@ -6,7 +6,9 @@ class Pet { // creamos una clase Pet
   final int age;
   final String image;
   final String type;
+  final int typeId;
   final String status;
+  final int statusId;
 
   //asignamos las variables en el constructor
   Pet({
@@ -16,7 +18,9 @@ class Pet { // creamos una clase Pet
     this.age,
     this.image,
     this.type,
-    this.status
+    this.typeId,
+    this.status,
+    this.statusId
   });
 
   /* mapeamos la respuesta para usarla más facilmente
@@ -29,9 +33,22 @@ class Pet { // creamos una clase Pet
       desc: json['desc'],
       age: json['age'],
       image: json['image'] ?? 'logo_flutter.png',
+      typeId: json['type']['id'],
       type: json['type']['name'],
       status: json['status']['name']
     );
+  }
+
+  Map toMap() {
+    var map = Map<String, dynamic>();
+    map['name'] = name;
+    map['desc'] = desc;
+    map['age'] = age.toString();
+    map['image'] = image;
+    map['typeId'] = typeId.toString();
+    map['statusId'] = statusId.toString();
+ 
+    return map;
   }
 }
 
--- lib/routes/AddPetPage.dart
+++ lib/routes/AddPetPage.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
 import 'package:image_picker/image_picker.dart';
+
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:http/http.dart' as http;
+import 'package:lab_02/models_api/Pet.dart';
+import 'package:lab_02/main.dart';
 
 class AddPetPage extends StatelessWidget {
   @override
@@ -22,23 +28,35 @@ class FormAddPet extends StatefulWidget {
 }
 
 class FormAddPetState extends State<FormAddPet> {
-  //añadimos un List con el tipo de opciones que necesitamos mostrar
-  List<String> _types = ['Perrito', 'Gatito'];
   //declaramos una variable donde guardaremos el item seleccionado
   String _selectedType = 'Por favor escoge';
+  String _selectedTypeId;
   //variable auxiliar SwitchListTile
   bool _rescue = false;
   //image file for picker image
   File _imageFile;
+  ProgressDialog pr;
 
   //global key para validar
   GlobalKey<FormState> _formKey = GlobalKey();
+
+  bool _validate = false;
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  List<PetKeyValue> _data = [
+    PetKeyValue(key: 'Perrito', value: '1'),
+    PetKeyValue(key: 'Gatito', value: '2'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(//creamos una vista scrolleable
       child: Form(//añadimos un form
         key: _formKey, //añadimos la llave a nuestro form
+        autovalidate: _validate,
         child: _form(),//llamamos desde aqui la funcion que construye el form
       ),
     );
@@ -50,6 +68,7 @@ class FormAddPetState extends State<FormAddPet> {
       child: Column(//column para multiples hijos
         children: <Widget>[//array
           TextFormField(//text form field para validar
+            controller: titleController,
             decoration: InputDecoration(
               icon: Icon(Icons.pets),//añadimos un icono
               hintText: 'Nombre', //placeholder
@@ -60,6 +79,7 @@ class FormAddPetState extends State<FormAddPet> {
             validator: (value) => _validReq(value, 'Coloca un nombre a tu amigo'),
           ),
           TextFormField(//text form field para validar
+            controller: descriptionController,
             //keyboard type multiline para escribir texto largo
             keyboardType: TextInputType.multiline,
             maxLines: null,//definimos null para no poner limites
@@ -72,6 +92,7 @@ class FormAddPetState extends State<FormAddPet> {
             validator: (value) => _validReq(value, 'Agrega una descripción'),
           ),
           TextFormField(//text form field para validar
+            controller: ageController,
             //input de tipo numérico
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
@@ -84,18 +105,19 @@ class FormAddPetState extends State<FormAddPet> {
           ),
           Container(//agregamos un container para manejar espacios
             padding: EdgeInsets.only(left: 5.0, top: 10.0),//padding left y top
-            child: DropdownButton<String>(//Declaramos el widget dropdown
+            child: DropdownButton<PetKeyValue>(//Declaramos el widget dropdown
               hint: Text(_selectedType),//texto placeholder
               isExpanded: true,//expandimos el elemento al 100%
-              items: _types.map((String value) {//mapeamos el array de tipos
-                return DropdownMenuItem(//retornamos cada item
-                  value: value,//valor
-                  child: Text(value),//texto del valor
+              items: _data.map((data) {//mapeamos el array de tipos
+                return DropdownMenuItem<PetKeyValue>(//retornamos cada item
+                  value: data,//valor
+                  child: Text(data.key),//texto del valor
                 );
               }).toList(),//convertimos en lista
-              onChanged: (String newValue) {//evento change
+              onChanged: (PetKeyValue value) {//evento change
+                _selectedType = value.key;
                 setState(() {//seteamos el estado
-                  _selectedType = newValue;//cambiamos el elemento seleccionado
+                  _selectedTypeId = value.value;//cambiamos el elemento seleccionado
                 });
               },
             ),
@@ -173,7 +195,6 @@ class FormAddPetState extends State<FormAddPet> {
     return FutureBuilder<File>(//retornamos un future builder de la imagen
       builder: (context, snapshot) {//snapshot de la imagen seleccionada
         return Container(//contenedor
-          padding: EdgeInsets.all(20.0),//padding
           child: _imageFile == null //si la imagen es nula
                 ? Text ('Seleccionar imagen')//colocamos un texto
                 //si no es nula retornamos la imagen seleccionada
@@ -184,7 +205,28 @@ class FormAddPetState extends State<FormAddPet> {
   }
 
   void _validateForm () {
-    _formKey.currentState.validate();
+    if (_formKey.currentState.validate()) {
+      pr = new ProgressDialog(context, 
+        type: ProgressDialogType.Normal,
+        isDismissible: false,
+      );
+      pr.show();
+
+      Pet newPet = Pet(
+        name: titleController.text,
+        desc: descriptionController.text,
+        age: (ageController.text != '') ? int.parse(ageController.text) : 0,
+        image: _getImage(),
+        typeId: int.parse(_selectedTypeId),
+        statusId: (_rescue) ? 2 : 1,
+      );
+
+      createPost('http://pets.memoadian.com/api/pets', body: newPet.toMap());
+    } else {
+      setState(() {
+        _validate = true;
+      });
+    }
   }
 
   String _validReq (String value, String message) {
@@ -203,4 +245,34 @@ class FormAddPetState extends State<FormAddPet> {
       return null;//retornamos null
     }
   }
+
+  String _getImage () {
+    if (_imageFile == null) return '';
+    String base64Image = base64Encode(_imageFile.readAsBytesSync());
+    return base64Image;
+  }
+
+  void createPost(String url, {Map body}) async {
+    return http.post(url, body: body).then((http.Response response) {
+      final int statusCode = response.statusCode;
+  
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        print(response.body);
+        pr.dismiss();
+        throw new Exception("Error while fetching data"+response.body);
+      }
+      Navigator.push(context,
+        MaterialPageRoute(
+          builder: (context) => MyApp(),
+        ),
+      );
+    });
+  }
+}
+
+class PetKeyValue {
+  String key;
+  String value;
+
+  PetKeyValue({this.key, this.value});
 }
