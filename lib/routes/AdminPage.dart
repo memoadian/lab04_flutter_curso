2fb2344ba563827381d2229251aa48eb5238a062
memoadian <memoadian@gmail.com>
2019-11-20 18:14:34 -0600

    list pets admin

--- lib/routes/AdminPage.dart
+++ lib/routes/AdminPage.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:lab_02/routes/AddPetPage.dart';
 import 'package:lab_02/routes/EditPetPage.dart';//importamos Añadir amigo
+import 'package:lab_02/models_api/Pet.dart';//importamos el mdoelo Pet.dart+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class AdminPage extends StatelessWidget {
+class AdminPage extends StatefulWidget{
+  @override
+  createState() => AdminPageState();
+}
+
+class AdminPageState extends State<AdminPage>{
+  //declaramos la variable que guardará la lista de elementos
+  List<Pet> _pets = List<Pet>();
+
+  @override
+  void initState() {
+    super.initState();
+    getPets();
+  }
+
+  // declaramos la funcion de tipo Future Null asincrona
+  Future<Null> getPets() async {
+    //consumimos el webservice con la librería http y get
+    final response = await http.get('http://pets.memoadian.com/api/pets/');
+
+    //si la respuesta es correcta responderá con 200
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);//guardamos la respuesta en json
+      /* accedemos al array data que es el que nos interesa
+       * y lo guardamos en una variable de tipo Iterable
+       */
+      Iterable list = result['data'];
+      setState(() {//seteamos el estado para actualizar los cambios
+        //mapeamos la lista en modelos Pet
+        print(list.map((model) => Pet.fromJson(model)).toString());
+        _pets = list.map((model) => Pet.fromJson(model)).toList();
+      });
+    } else {
+      throw Exception('Fallo al cargar información del servidor');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     /*
@@ -85,38 +124,52 @@ class AdminPage extends StatelessWidget {
   }
 
   Widget server (BuildContext context) {
-    return ListView(//cambiamos el texto por un listView
-      children: <Widget>[//array
-        Divider(height: 15.0),//espacio por encima del primer listview
-        Card(//card
-          margin: EdgeInsets.all(5.0),//margen
-          child: ListTile(//Listile para ordenar
-            title: Text('Amigo'),//titulo
-            subtitle: Text('Edad: 0 años'),//subtitulo
-            leading: Image.asset('images/logo_flutter.png'),//icono
-            trailing: Row(//Row para acomodar iconos al final
-              mainAxisSize: MainAxisSize.min,//ordenamiento horizontal
-              children: <Widget>[//array
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                        //navegar a editar amigo
-                        builder: (context) => EditPetPage()
-                      ),
-                    );
-                  },
-                ),
-                IconButton(//icono con botón
-                  icon: Icon(Icons.delete),//icono
-                  onPressed: () {},//evento press eliminar
-                ),
-              ],
-            ),
-          ),
+    return ListView.builder(//listview builder
+      itemCount: _pets.length,
+      itemBuilder: _petsBuilder,
+    );
+  }
+
+  Widget _petsBuilder (BuildContext context, int pos) {
+    return Card(//card
+      margin: EdgeInsets.all(5.0),//margen
+      child: ListTile(//Listile para ordenar
+        title: Text(_pets[pos].name),//tacomodaritulo
+        subtitle: Text('Edad: ${_pets[pos].age}'),//subtitulo
+        leading: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(0)),
+            ClipRRect(
+              borderRadius: new BorderRadius.circular(100.0),
+              child: Image.network(
+                  _pets[pos].image,
+                  height: 50.0,
+                  width: 50.0,
+              ),
+            )
+          ],
         ),
-      ]
+        trailing: Row(//Row para  iconos al final
+          mainAxisSize: MainAxisSize.min,//ordenamiento horizontal
+          children: <Widget>[//array
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.push(context,
+                  MaterialPageRoute(
+                    //navegar a editar amigo
+                    builder: (context) => EditPetPage()
+                  ),
+                );
+              },
+            ),
+            IconButton(//icono con botón
+              icon: Icon(Icons.delete),//icono
+              onPressed: () {},//evento press eliminar
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
